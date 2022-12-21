fb5bc74e44c27f36990a8a90f05bbd9cce25bb86
roteKlaue <janoolhd@gmail.com>
2022-12-21 01:27:33 +0100

    added game container construct

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+Array.prototype.shuffle = function() {
+	let currentIndex = this.length,  randomIndex;
+	while (currentIndex != 0) {
+	  	randomIndex = Math.floor(Math.random() * currentIndex);
+	  	currentIndex--;
+	  	[this[currentIndex], this[randomIndex]] = [this[randomIndex], this[currentIndex]];
+	}
+}
+
 const diffi = document.querySelectorAll('button[group="difficulty"]');
 let difficulty = "easy";
 diffi.forEach(elm =>
