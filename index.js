67867900dc956ac09492d6857255a8b2cde46e1c
roteKlaue <janoolhd@gmail.com>
2023-01-03 00:37:32 +0100

    formatting

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ const request = (callback) => {
 }
 
 const loadQuestion = (index) => {
-	if(index >= state.currentQuesions.length) {
+	if (index >= state.currentQuesions.length) {
 		document.querySelector("#game").style.display = "none";
 		document.querySelector(".end").style.display = "block";
 		document.querySelector("#total").innerText = state.currentQuesions.length;
@@ -49,12 +49,12 @@ const loadQuestion = (index) => {
 	const currentQuestion = state.currentQuesions[index];
 	state.sus = true;
 	document.querySelector("#go").innerText = "submit";
-	awnsers.forEach(e => {e.removeAttribute("disabled");e.removeAttribute("state");e.addEventListener("click", highlight)});
+	awnsers.forEach(e => { e.removeAttribute("disabled"); e.removeAttribute("state"); e.addEventListener("click", highlight) });
 	awnsers.shuffle();
 
 	question.innerText = currentQuestion.question;
 	awnsers[0].innerText = currentQuestion.correctAnswer;
-	for(let i = 1; i < awnsers.length; i++) {
+	for (let i = 1; i < awnsers.length; i++) {
 		awnsers[i].innerText = currentQuestion.incorrectAnswers[i - 1];
 	}
 }
@@ -66,21 +66,21 @@ document.querySelector("#limit").addEventListener("input", (e) => {
 document.querySelector("#sus").innerText = "05";
 
 document.querySelector("#start").addEventListener("click", request.bind(null, (questions) => {
-		document.querySelector("#pregame").style.display = "none";
-		document.querySelector("#game").style.display = "block";
-		state.currentQuesions = questions;
-		document.querySelector("#score").innerHTML = '<span class="square gray"></span>'.repeat(questions.length);
-		loadQuestion(0);
-	}));
+	document.querySelector("#pregame").style.display = "none";
+	document.querySelector("#game").style.display = "block";
+	state.currentQuesions = questions;
+	document.querySelector("#score").innerHTML = '<span class="square gray"></span>'.repeat(questions.length);
+	loadQuestion(0);
+}));
 
-document.querySelector("#go").addEventListener("click", () => 
-	(state.sus? () => {
-		if(!state.selected) return;
+document.querySelector("#go").addEventListener("click", () =>
+	(state.sus ? () => {
+		if (!state.selected) return;
 		const correct = awnsers.find(e => e.innerHTML.toLowerCase() == state.currentQuesions[state.currentIndex].correctAnswer.toLowerCase());
 		correct.setAttribute("state", "correct");
-		document.querySelectorAll(".square")[state.currentIndex].classList.add(correct.id !== state.selected? "red": "green");
+		document.querySelectorAll(".square")[state.currentIndex].classList.add(correct.id !== state.selected ? "red" : "green");
 		state.correct++;
-		if(correct.id !== state.selected) {
+		if (correct.id !== state.selected) {
 			document.querySelector(`#${state.selected}`).setAttribute("state", "wrong");
 			state.correct--;
 			state.wrong++;
